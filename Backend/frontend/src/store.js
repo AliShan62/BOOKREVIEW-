be79f2fbcf5c137c28d5a270b87e7d17fb8ce565
Ali Shan <alishanit62@gmail.com>
2024-08-28 12:01:15 +0500

    hallo

--- Backend/frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers'; // Import your combined reducers
-
-const store = configureStore({
-  reducer: rootReducer,
-  // Add middleware, enhancers, or other configurations here if needed
-});
-
-export default store;
